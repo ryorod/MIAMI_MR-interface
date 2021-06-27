1b9f25c17aa42070b7fa679320b0793a18142c06
ryorod <ryo3ishi28@gmail.com>
2021-06-28 05:23:09 +0900

    init

---
+++ Assets/Scripts/FingerTracker.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Microsoft;
+using Microsoft.MixedReality.Toolkit.Utilities;
+using Microsoft.MixedReality.Toolkit.Input;
+
+// https://www.benjaminoutram.com/blog/2020/1/28/hololens-2-development-with-unity-hand-tracking-and-accessing-finger-positions
+
+public class FingerTracker : MonoBehaviour
+{
+    public GameObject fingerObject;
+    private MixedRealityPose pose;
+    private OSCController osc;
+
+    void Start()
+    {
+        this.osc = GetComponent<OSCController>();
+        this.osc.Init();
+    }
+
+    void Update()
+    {
+        // only render if hand is tracked
+        this.fingerObject.GetComponent<Renderer>().enabled = false;
+
+        if (HandJointUtils.TryGetJointPose(TrackedHandJoint.IndexTip, Handedness.Left, out pose))
+        {
+            this.fingerObject.transform.position = pose.Position;
+            this.fingerObject.GetComponent<Renderer>().enabled = true;
+
+            Vector3 pos = this.fingerObject.transform.position;
+            string fingerPos = pos.x.ToString() + " " + pos.y.ToString() + " " + pos.z.ToString();
+            this.osc.Send("/z", fingerPos);
+        }
+    }
+}
---
+++ Assets/Scripts/OSCController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OSCController : MonoBehaviour
+{
+    public string serverId = "MaxMSP";
+    public string serverIp = "127.0.0.1";
+    public int    serverPort = 12000;
+
+    public void Init()
+    {
+        OSCHandler.Instance.Init(this.serverId, this.serverIp, this.serverPort);
+    }
+
+    public void Send(string adr, string msg)
+    {
+        OSCHandler.Instance.SendMessageToClient(this.serverId, adr, msg);
+    }
+}
