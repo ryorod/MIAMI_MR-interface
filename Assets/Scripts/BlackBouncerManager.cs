5c06b6a60faa092e403f14c2536f3d99679fc9e7
ryorod <ryo3ishi28@gmail.com>
2022-01-14 13:53:29 +0900

    update

---
+++ Assets/Scripts/BlackBouncerManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackBouncerManager : MonoBehaviour
+{
+    private OSCController osc;
+
+    void Start()
+    {
+        this.osc = GameObject.Find("MixedRealityPlayspace").GetComponent<OSCController>();
+        this.osc.InitM4L();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Vector3 pos = this.transform.position;
+        Vector3 velXYZ = this.gameObject.GetComponent<Rigidbody>().velocity;
+        float vel = (velXYZ.x + velXYZ.y + velXYZ.z) / 3f;
+        string msg = pos.x.ToString() + " " + pos.y.ToString() + " " + pos.z.ToString() + " " +
+                     vel.ToString();
+        this.osc.SendM4L("/" + this.gameObject.name, msg);
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        this.osc.SendM4L("/" + this.gameObject.name + "_release", "");
+    }
+}
--- Assets/Scripts/OSCController.cs
+++ Assets/Scripts/OSCController.cs
@@ -4,17 +4,29 @@ using UnityEngine;
 
 public class OSCController : MonoBehaviour
 {
+    public string receiverIp = "127.0.0.1";
     public string serverId = "MusicVAE";
-    public string serverIp = "127.0.0.1";
     public int    serverPort = 12000;
+    public string M4LId = "M4L";
+    public int M4LPort = 13000;
 
     public void Init()
     {
-        OSCHandler.Instance.Init(this.serverId, this.serverIp, this.serverPort);
+        OSCHandler.Instance.Init(this.serverId, this.receiverIp, this.serverPort);
     }
 
     public void Send(string adr, string msg)
     {
         OSCHandler.Instance.SendMessageToClient(this.serverId, adr, msg);
+    }
+
+    public void InitM4L()
+    {
+        OSCHandler.Instance.Init(this.M4LId, this.receiverIp, this.M4LPort);
+    }
+
+    public void SendM4L(string adr, string msg)
+    {
+        OSCHandler.Instance.SendMessageToClient(this.M4LId, adr, msg);
     }
 }
