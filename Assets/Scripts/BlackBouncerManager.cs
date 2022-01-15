364d6b2c31bc286da675392462e1cd5ae7647596
ryorod <ryo3ishi28@gmail.com>
2022-01-15 19:29:55 +0900

    update

--- Assets/Scripts/BlackBouncerManager.cs
+++ Assets/Scripts/BlackBouncerManager.cs
@@ -14,11 +14,15 @@ public class BlackBouncerManager : MonoBehaviour
 
     void OnCollisionEnter(Collision collision)
     {
+        Vector3 userPos = Camera.main.transform.position;
         Vector3 pos = this.transform.position;
         Vector3 velXYZ = this.gameObject.GetComponent<Rigidbody>().velocity;
-        float vel = (velXYZ.x + velXYZ.y + velXYZ.z) / 3f;
+        float vel = Mathf.Max(Mathf.Abs(velXYZ.x), Mathf.Max(Mathf.Abs(velXYZ.y), Mathf.Abs(velXYZ.z)));
         string msg = pos.x.ToString() + " " + pos.y.ToString() + " " + pos.z.ToString() + " " +
                      vel.ToString();
+        string userPosMsg = userPos.x.ToString() + " " + userPos.y.ToString() + " " + userPos.z.ToString();
+        
+        this.osc.SendM4L("/user_pos", userPosMsg);
         this.osc.SendM4L("/" + this.gameObject.name, msg);
     }
 
